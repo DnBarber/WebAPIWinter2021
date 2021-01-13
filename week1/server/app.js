1638b36e9602d95e80546c10fea5ad8a12af5b51
DnBarber <dtbarber@email.neit.edu>
2021-01-13 13:08:29 -0500

    third commit

--- week1/server/app.js
+++ week1/server/app.js
@@ -1,7 +1,13 @@
 //App is the entry point of application 
 
-function sendMsg(){
-    console.log("Hello World?");
-}
+var http = require('http');
 
-sendMsg();
+http.createServer(function(request,response){
+    //http header
+    response.writeHead(200,{'content-type':'text/plain'});
+
+    //send a response to the body of the html
+    response.end("Wouldn't you like to be a pepper too?");
+}).listen(3000);
+
+console.log("Server is running on port 3000");
